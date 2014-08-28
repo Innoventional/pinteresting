193e817614bc8f4900621f48a630073116e3a37f
Inchorify <jonathan.erwin@inchora.com>
2014-08-28 22:08:49 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = '30eb3002d25bb7b0ab62da9f27c3ab32f559d4bc837400e9d3045201c3d95e3d7ed24f995c93f60edeb65260320cc5a60ac5b7d098598d59c596c7751c5b9702'
